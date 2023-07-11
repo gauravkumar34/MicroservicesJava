9739f92805aad3f89051c1a41cac512285a70616
Gaurav <gk461139@gmail.com>
2023-07-12 03:48:36 +0530

    add okta security

--- HotelService/src/main/java/com/hotelservices/hotel/repositories/HotelRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hotelservices.hotel.repositories;
-
-import com.hotelservices.hotel.entities.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HotelRepository extends JpaRepository<Hotel, String> {
-}
